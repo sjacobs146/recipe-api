df769e5da9362917f3096376ae1f98893a6d0fbe
sjacobs146 <susanjacobs@verizon.net>
2017-09-25 11:14:02 -0400

    Add production path
    Add production path (heroku) to scripts in commented form

--- scripts/create-recipe.sh
+++ scripts/create-recipe.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# TOKEN=BAhJIiVkNGFmMDk4ZWFiN2Y0NmUyZDE0NGRhYmU1ODA2NjA2YgY6BkVG--a7ebd1ae5a689f8eb4e13826a904206fa360bc95 NAME="Chocolate Cake" sh create-recipe.sh
+# TOKEN=BAhJIiU1YzI2MmEyM2UxMjE0NmRjZThmZjJlMWJjYzYzMjMyZgY6BkVG--02a11f2b646b0a2794ea4d5ff4a248e086ab3597 NAME="Chocolate Cake" sh scripts/create-recipe.sh
 
 API="${API_ORIGIN:-http://localhost:4741}"
+# API="${API_ORIGIN:-https://sjacobs146-recipe-api.herokuapp.com}"
 URL_PATH="/recipes"
 curl "${API}${URL_PATH}" \
   --include \
--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
+/API="${API_ORIGIN:-https://sjacobs146-recipe-api.herokuapp.com}"
 URL_PATH="/sign-out"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
